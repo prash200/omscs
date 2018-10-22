2318afd1234d6b96fa5fe7d3b22d935328ff204e
Prashant Agrawal <pra@microsoft.com>
2018-10-22 13:38:48 -0700

    add

--- 6210/hw2/gtmpi.c
+++ 6210/hw2/gtmpi.c
@@ -8,11 +8,11 @@ int main(int argc, char **argv)
 {
   printf("This is the serial section\n");
 
+  MPI_Init(&argc, &argv);
+
   int num_processes;
   MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
   gtmpi_init(num_processes);
-
-  MPI_Init(&argc, &argv);
 
   int my_id;
   MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
