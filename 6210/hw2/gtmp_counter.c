8ebe56f14304e74aaf49d258e0288c18ccd7df0d
Prashant Agrawal <pra@microsoft.com>
2018-10-22 10:08:32 -0700

    changes

--- 6210/hw2/gtmp_counter.c
+++ 6210/hw2/gtmp_counter.c
@@ -9,7 +9,7 @@ static short *sense;
 void gtmp_init(int n_threads)
 {
   count = num_threads = n_threads;
-  // To avoid false sharing, allocate a block equal to the chache line size and alligned to chache line.
+  // To avoid false sharing, allocate a block equal to the chache line size.
   posix_memalign((void**)&sense, LEVEL1_DCACHE_LINESIZE, LEVEL1_DCACHE_LINESIZE);
   *sense = 0;
 }
@@ -17,18 +17,18 @@ void gtmp_init(int n_threads)
 void gtmp_barrier()
 {
   short *local_sense;
-  // To avoid false sharing, allocate a block equal to the chache line size and alligned to chache line.
+  // To avoid false sharing, allocate a block equal to the chache line size.
   posix_memalign((void**)&local_sense, LEVEL1_DCACHE_LINESIZE, LEVEL1_DCACHE_LINESIZE);
   *local_sense = *sense ^ 0x1;
 
   if (__sync_fetch_and_sub(&count, 1) == 1)
   {
     count = num_threads;
-    sense = *local_sense;
+    *sense = *local_sense;
   }
   else
   {
-    while (sense != *local_sense);
+    while (*sense != *local_sense);
   }
 
   free((void*)local_sense);
