99b1d777a9bc77bdfb408ecf66d6777b96c9226a
Prashant Agrawal <pra@microsoft.com>
2018-10-22 17:07:43 -0700

    changes

--- 6210/hw2/gtmp_mcs.c
+++ 6210/hw2/gtmp_mcs.c
@@ -53,20 +53,17 @@ void gtmp_barrier()
   printf("thread %d\n", thread_num);
 
   printf("nodes[%d].child_not_ready %d\n", thread_num, nodes[thread_num].child_not_ready);
-  while (nodes[thread_num].child_not_ready != 0)
-  {
-    //printf("nodes[%d].child_not_ready %d\n", thread_num, nodes[thread_num].child_not_ready);
-  }
+  while (nodes[thread_num].child_not_ready != 0);
 
   nodes[thread_num].child_not_ready = nodes[thread_num].have_child;
   printf("nodes[%d].child_not_ready %d\n", thread_num, nodes[thread_num].child_not_ready);
 
-  nodes[(thread_num - 1) / 4].child_not_ready &= ~(1 << ((thread_num + 3) % 4));
-  printf("nodes[%d].child_not_ready %d\n", (thread_num - 1) / 4, nodes[(thread_num - 1) / 4].child_not_ready);
-
   unsigned short local_sense = nodes[thread_num].sense ^ 0x1;
   if (thread_num != 0)
   {
+    nodes[(thread_num - 1) / 4].child_not_ready &= ~(1 << ((thread_num + 3) % 4));
+    printf("nodes[%d].child_not_ready %d\n", (thread_num - 1) / 4, nodes[(thread_num - 1) / 4].child_not_ready);
+
     while (nodes[thread_num].sense != local_sense);
   }
 
